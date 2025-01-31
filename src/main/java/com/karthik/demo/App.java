a10475abcca143f80f2bbae2474fb9969a1df1f0
Karthik <karthikeeyan.srinivasan@gmail.com>
2025-01-31 13:04:48 +0530

    second

--- src/main/java/com/karthik/demo/App.java
+++ src/main/java/com/karthik/demo/App.java
@@ -1,13 +1,35 @@
 package com.karthik.demo;
 
+
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static void addData(String Data)
+	{
+		System.out.println("Data "+Data);
+		if(Data.equals("Karthikeeyan"))
+		{
+			System.out.println("Welcome");
+		}
+	}
+	public static void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}
     public static void main( String[] args )
     {
+    	Scanner sc = new Scanner(System.in);
+    	String Data = sc.next();
+    	int a = 5;
+    	int b = 10;
+    	add(a,b);
+    	addData(Data);
         System.out.println( "Hello World!" );
     }
 }
+
